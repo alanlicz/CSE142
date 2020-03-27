fa39e207d251fa27e1fad7b690768ca2ad435e92
alanlicz <alanltycz@icloud.com>
2020-03-27 12:14:00 -0700

    Assignment#2 modification

--- src/Budgeter.java
+++ src/Budgeter.java
@@ -1,3 +1,10 @@
+// Alan Li
+// 01/26/2020
+// CSE142
+// TA: Eric Koegler
+// Assignment #3
+//
+// This program will output a cafe wall illusion image
 import java.util.*;
 
 public class Budgeter {
@@ -6,9 +13,10 @@ public class Budgeter {
     public static final int DAYS = 31;
 
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
         welcomeMessage();
-        double totalIncome = getIncome();
-        double totalExpense = getExpense();
+        double totalIncome = getIncome(console);
+        double totalExpense = getExpense(console);
         double netIncome = totalIncome - totalExpense;
         double dailyIncome = totalIncome / DAYS;
         double dailyExpense = totalExpense / DAYS;
@@ -22,52 +30,48 @@ public class Budgeter {
         System.out.println();
     }
 
-    // Allow user type their income entries and calculate the total income
-    public static double getIncome() {
-        Scanner console = new Scanner(System.in);
-        System.out.print("How many categories of income? ");
+    // Calculate the total expense or income
+    public static double calculation(Scanner console, String type){
         int count = console.nextInt();
         double sum = 0;
         for (int i = 1; i <= count; i++) {
-            System.out.print("    Next income amount? $");
+            System.out.printf("    Next %s amount? $", type);
             double amount = console.nextDouble();
             sum = sum + amount;
         }
         return sum;
     }
+    // Allow user type their income entries and calculate the total income
+    public static double getIncome(Scanner console) {
+        System.out.print("How many categories of income? ");
+        return calculation(console, "income");
+    }
 
     // Allow user type their expense either by daily or monthly, then calculate the total expense
-    public static double getExpense() {
-        Scanner console = new Scanner(System.in);
+    public static double getExpense(Scanner console) {
         System.out.println();
         System.out.print("Enter 1) monthly or 2) daily expenses? ");
         int choice = console.nextInt();
         System.out.print("How many categories of expense? ");
-        int count = console.nextInt();
-        double sum = 0;
-        double result = 0;
-        for (int i = 1; i <= count; i++) {
-            System.out.print("    Next expense amount? $");
-            double amount = console.nextDouble();
-            sum = sum + amount;
-            if (choice == 1) {
-                result = sum;
-            }
-            else{
-                result = sum * DAYS;
-            }
+        double result;
+        if (choice == 1) {
+            result = calculation(console, "expense");
+        }
+        else{
+            result = calculation(console, "expense") * DAYS;
         }
         return result;
     }
 
-    // Print out the total expenses and income as long as which category does the user fall into(saver or spender)
-    // double netIncome -variable that used to calculate the netIncome according to the total income and expense
+    // Print out the total expenses and income as long as which category does the user fall into
+    // (saver or spender) double netIncome -variable that used to calculate the netIncome according
+    // to the total income and expense
     // double dailyIncome -variable that used to calculate daily income for output
     // double dailyExpense -variable that used to calculate daily expense for output
     // double totalIncome -variable that used to calculate the total income
     // double totalExpense -variable that used to calculate the total expense
-    public static void printMessage(double netIncome, double dailyIncome, double dailyExpense, double totalIncome,
-                                    double totalExpense){
+    public static void printMessage(double netIncome, double dailyIncome, double dailyExpense,
+                                    double totalIncome, double totalExpense){
         System.out.println();
         System.out.printf("Total income = $%.2f ($%.2f/day)\n", totalIncome, dailyIncome);
         System.out.printf("Total expenses = $%.2f ($%.2f/day)\n", totalExpense, dailyExpense);
